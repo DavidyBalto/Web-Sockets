c15afa5243841dbb986445bc1325432f103e4549
DavidyBalto <david1123proa@gmail.com>
2024-11-20 12:48:33 -0500

    Correccion en scroll chat

--- server/expres.js
+++ server/expres.js
@@ -2,7 +2,7 @@ import expres from 'express'
 
 import {Server} from 'socket.io'
 import { createServer } from 'node:http'
-import { ChatModel } from '../models/TursoDB.js'
+import { ChatModel } from '../models/SQLocal.js'
 
 const port = process.env.PORT ?? 1234
 
